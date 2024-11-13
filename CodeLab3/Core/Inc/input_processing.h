4de0d39ab04c6ee8158ffc9809e8fa82f70f7351
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2024-11-13 16:46:56 +0700

    no message

--- CodeLab3/Core/Inc/input_processing.h
+++ CodeLab3/Core/Inc/input_processing.h
@@ -2,7 +2,7 @@
 #define __INPUT_PROCESSING_H_
 
 #include "main.h"
-#include "input_reading.h"
+//#include "input_reading.h"
 #include "led_display.h"
 #include "timer.h"
 #include "global.h"
@@ -10,5 +10,13 @@
 void UpdateMode();
 void LedTimeDurationInit();
 void UpdateDurationValue();
+
+//---------------------------input_reading (đọc nút nhấn và chống nhiễu)----------
+void init_buffer();
+void button_reading(void);
+unsigned char is_button_pressed(unsigned char index);
+unsigned char is_button_pressed_1s(unsigned char index);
+unsigned char is_button_held(unsigned char index);
+void reset_flagForButtonHold(unsigned char index);
 
 #endif /* INC_INPUT_PROCESSING_H_ */
--- CodeLab3/Debug/Core/Src/input_processing.d
+++ CodeLab3/Debug/Core/Src/input_processing.d
@@ -24,8 +24,8 @@ Core/Src/input_processing.o: ../Core/Src/input_processing.c \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/input_reading.h ../Core/Inc/timer.h \
- ../Core/Inc/led_display.h ../Core/Inc/global.h
+ ../Core/Inc/led_display.h ../Core/Inc/timer.h \
+ ../Core/Inc/input_reading.h ../Core/Inc/global.h
 
 ../Core/Inc/input_processing.h:
 
@@ -79,10 +79,10 @@ Core/Src/input_processing.o: ../Core/Src/input_processing.c \
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 
-../Core/Inc/input_reading.h:
+../Core/Inc/led_display.h:
 
 ../Core/Inc/timer.h:
 
-../Core/Inc/led_display.h:
+../Core/Inc/input_reading.h:
 
 ../Core/Inc/global.h:
